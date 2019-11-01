03a6255cc55ba22e8d84ecfaaaa8b1c7f4690f82
hison731 <jinxing4@outlook.com>
2019-11-01 16:50:22 -0700

    final

--- src/app/page5/page5.page.ts
+++ src/app/page5/page5.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {NavController} from '@ionic/angular';
 
 @Component({
   selector: 'app-page5',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Page5Page implements OnInit {
 
-  constructor() { }
+  constructor(private navCtrl: NavController) { }
 
   ngOnInit() {
+  }
+
+  back() {
+    this.navCtrl.back({animated: false});
   }
 
 }
