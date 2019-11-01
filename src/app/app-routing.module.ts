eef3a482eed1ecb700f660493d5ee80b43f269c3
hison731 <jinxing4@outlook.com>
2019-10-31 17:13:40 -0700

    homepage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
+    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
   },
   {
     path: 'home',
@@ -14,7 +13,11 @@ const routes: Routes = [
   {
     path: 'list',
     loadChildren: () => import('./list/list.module').then(m => m.ListPageModule)
-  }
+  },
+  { path: 'page1', loadChildren: './page1/page1.module#Page1PageModule' },
+  { path: 'page2', loadChildren: './page2/page2.module#Page2PageModule' },
+  { path: 'page3', loadChildren: './page3/page3.module#Page3PageModule' },
+  { path: 'page4', loadChildren: './page4/page4.module#Page4PageModule' },
 ];
 
 @NgModule({
